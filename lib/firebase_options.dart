9ab2ae8e50fd54ba7e84e2072c6615f0a8f020b3
Chinmay <chinmaywadettiwar211@gmail.com>
2026-03-09 23:00:12 +0530

    vercel config added

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -65,4 +62,14 @@ class DefaultFirebaseOptions {
     storageBucket: 'basketorders-9a569.firebasestorage.app',
     iosBundleId: 'com.example.basketorders',
   );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyAyVN50rBKEd0CMMKidbmxdv3Lb5fPM4Gg',
+    appId: '1:958623060295:web:25de95ddee3a3af6df7c62',
+    messagingSenderId: '958623060295',
+    projectId: 'basketorders-9a569',
+    authDomain: 'basketorders-9a569.firebaseapp.com',
+    storageBucket: 'basketorders-9a569.firebasestorage.app',
+  );
+
 }
